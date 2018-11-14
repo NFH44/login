9c010c8ff4a07477df331c6b6f14ab52aa0bc3d5
pengpeng <F904832978@126.com>
2018-11-14 09:38:12 +0800

    分页

--- src/main/java/com/test/dao/ShipDao.java
+++ src/main/java/com/test/dao/ShipDao.java
@@ -1,26 +1,15 @@
 package com.test.dao;
 
-import com.test.entity.Page;
 import com.test.entity.Ship;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ShipDao {
-    int pageCount();
     void insert(Ship ship);
-    List<Ship> findAll(@Param("id") int id,
-                       @Param("page") Page page);
     void update(Ship ship);
     void delete(int id);
-    List<Ship> findByNaionIdAndShipName(@Param("id") int id,
-                                        @Param("shipName") String shipName);
-    int findCountAndShipName(@Param("id") int id,
-                             @Param("shipName") String shipName);
-    List<Ship> findByNaionIdAndLv(@Param("id") int id,
-                                        @Param("Lv") int Lv);
-    List<Ship> findByNaionIdAndShipType(@Param("id") int id,
-                                        @Param("shipType") String ShipType);
-    List<Ship> findByNaionIdAndNote(@Param("id") int id,
-                                        @Param("note") String note);
+    List<Ship> findAll(@Param("queryMap") Map<String, Object> queryMap);
+    Integer queryAllCount(@Param("queryMap") Map<String, Object> queryMap);
 }
